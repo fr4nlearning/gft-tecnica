a837337aa52bd63e630c6a74632a8c3765d60391
Fran <francis.gestion@gmail.com>
2024-11-17 17:46:17 +0100

    fix&improvement: cambiado formato fecha, nuevos test para nuevo controll

--- inix/src/test/java/com/example/inix/infrastructure/rest/PriceControllerTest.java
+++ inix/src/test/java/com/example/inix/infrastructure/rest/PriceControllerTest.java
@@ -2,6 +2,7 @@ package com.example.inix.infrastructure.rest;
 
 import com.example.inix.application.services.PriceService;
 import com.example.inix.domain.model.DataRS;
+import com.example.inix.infrastructure.utils.PriceUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,8 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,14 +39,38 @@ class PriceControllerTest {
                 .price(BigDecimal.valueOf(25.45))
                 .build();
 
-        when(priceService.findByDateProductBrand("2020-06-14 16:00:00", 35455, 1))
+        LocalDateTime localDateTime= PriceUtils.fromStringToDate("2020-06-14-16.00.00");
+
+        when(priceService.findByDateProductBrand(localDateTime, 35455, 1))
                 .thenReturn(dataRS);
 
-        ResponseEntity<DataRS> result = priceController.findByDateProductBrand("2020-06-14 16:00:00", 35455, 1);
+        ResponseEntity<DataRS> result = priceController.findByDateProductBrand("2020-06-14-16.00.00", 35455, 1);
 
         assertEquals(HttpStatus.OK, result.getStatusCode());
         assertEquals(dataRS, result.getBody());
 
+    }
+
+    @Test
+    public void testFindProduct_IllegalArgumentException() {
+
+        assertThrows(IllegalArgumentException.class,
+                () -> priceController.findByDateProductBrand("2020-06-14T16.00.00", 35455, 1));
+    }
+
+    @Test
+    public void testFindProduct_NotFound() {
+
+        DataRS dataRS = null;
+
+        LocalDateTime localDateTime= PriceUtils.fromStringToDate("2020-06-14-16.00.00");
+
+        when(priceService.findByDateProductBrand(localDateTime, 35455, 1))
+                .thenReturn(dataRS);
+
+        ResponseEntity<DataRS> result = priceController.findByDateProductBrand("2020-06-14-16.00.00", 35455, 1);
+
+        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
     }
 
 }
