35be7331aafacc351bcf2cca8209b79f83b18d69
Fran <francis.gestion@gmail.com>
2024-11-18 01:39:42 +0100

    improvement: mejora de la documentacion

--- inix/src/main/java/com/example/inix/infrastructure/rest/IPriceController.java
+++ inix/src/main/java/com/example/inix/infrastructure/rest/IPriceController.java
@@ -1,9 +1,11 @@
 package com.example.inix.infrastructure.rest;
 
 import com.example.inix.domain.model.DataRS;
+import com.example.inix.infrastructure.dto.Error;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface IPriceController {
 
     @Operation(
+            operationId = "findByDateProductBrand",
             summary = "Find price by date, product ID, and brand ID",
             description = "Get data from database")
     @ApiResponses(value = {
@@ -26,21 +29,63 @@ public interface IPriceController {
                     description = "Successful response",
                     content = @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = DataRS.class))),
+                            schema = @Schema(implementation = DataRS.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                                {
+                                                    "startDate": "2020-06-14-00.00.00",
+                                                    "endDate": "2020-12-31-23.59.59",
+                                                    "priceList": 1,
+                                                    "productId": 35455,
+                                                    "brandId": 1,
+                                                    "price": 35.50
+                                                }
+                                            """
+                            )
+                    )),
             @ApiResponse(
                     responseCode = "400",
                     description = "Invalid input parameters",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Error.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                                {
+                                                    "httpStatus": "BAD_REQUEST",
+                                                    "localDateTime": "18/11/2024 01:09:47",
+                                                    "message": "Invalid format: "
+                                                }
+                                            """
+                            )
+                    )),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found",
                     content = @Content),
             @ApiResponse(
                     responseCode = "500",
                     description = "Internal Server Error",
-                    content = @Content),
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Error.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                                {
+                                                    "httpStatus": "INTERNAL_SERVER_ERROR",
+                                                    "localDateTime": "18/11/2024 01:09:47",
+                                                    "message": ""
+                                                }
+                                            """
+                            )
+                    ))
     })
+
 
     @GetMapping("/findBy")
     public ResponseEntity<DataRS> findByDateProductBrand(
-            @Parameter(description = "Date", required = true) @RequestParam String date,
-            @Parameter(description = "The Id of product", required = true) @RequestParam Integer productId,
-            @Parameter(description = "The Id of brand", required = true) @RequestParam Integer brandId
+            @Parameter(description = "Date", example = "2020-06-14-16.00.00", required = true) @RequestParam String date,
+            @Parameter(description = "The Id of product", example = "35455", required = true) @RequestParam Integer productId,
+            @Parameter(description = "The Id of brand", example = "1", required = true) @RequestParam Integer brandId
     );
 }
